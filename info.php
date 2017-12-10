e5afa05b37383d60170702b5fb70e1947117d328
unknown <Rokitanasz23@gmail.com>
2017-12-10 18:26:50 +0200

    asfasdf

--- info.php
+++ info.php
@@ -1,8 +1,15 @@
 <html>
 <head>
-    <title>Bandom php<title>
+    <title>Bendras darbas</title>
+    <meta charset="utf-8" />
 </head>
 <body>
-ysdfui gsfdu ygsuif guifs
+    Vardas: 
+<?php 
+echo $_GET['vardas'];
+?>
+<br />
+Atsakymas
+<?=$_GET['atsakymas'] ?>
 </body>
 </html>
