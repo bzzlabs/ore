a092e5982d0203d02706a4cb566611e5272b76be
anushauskas <32789479+anushauskas@users.noreply.github.com>
2017-12-10 18:14:52 +0200

    Atsakymas

--- info.php
+++ info.php
@@ -1,3 +1,12 @@
+<html>
+<head>
+    <title>Bandom php<title>
+</head>
+<body>
 <?php  
 echo "Ivestas vardas: " . $_GET['vardas'] 
 ?> 
+</br>
+Kazkoks tekstas <?php=$_GET['atsakymas']  ?>
+</body>
+</html>
