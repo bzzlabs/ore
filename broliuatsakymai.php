3ec947f533b233f59e80f2c2042d8ab88a3a15fa
Lurok <rokitanasz23@gmail.com>
2018-01-14 19:06:51 +0200

    '

--- broliuatsakymai.php
+++ broliuatsakymai.php
@@ -1,19 +1,20 @@
 <?php
 $failoVieta = $_SERVER['DOCUMENT_ROOT']."\\vaisiai.txt";
+echo $failoVieta;
 //$failas = fopen($failoVieta, "r");
 //$lines = file($failoVieta);
-$VisasFailas = file_get_contents('http://donatasa.azurewebsites.net/vaisiai.txt');
-$lines = explode("\r\n", $VisasFailas);
-foreach ($lines as $line_num => $line) {
-    $pos = strpos($line, 'mandarinas');
-    if ($pos === FALSE){
+// $VisasFailas = file_get_contents('http://donatasa.azurewebsites.net/vaisiai.txt');
+// $lines = explode("\r\n", $VisasFailas);
+// foreach ($lines as $line_num => $line) {
+//     $pos = strpos($line, 'mandarinas');
+//     if ($pos === FALSE){
 
-    } 
-    else{
-        echo $line "<br />\n";
-    }
-    // echo "Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
-}
+//     } 
+//     else{
+//         echo $line "<br />\n";
+//     }
+//     // echo "Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
+// }
     
 // Jei Pavyko, į failą įrašom kažkokią informaciją: 
 // Sitoj vietoj skaito faila po viena eilute
