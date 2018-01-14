19ddabb502a9b120f6ccc09926248da0ab98aa63
Lurok <rokitanasz23@gmail.com>
2018-01-14 16:25:47 +0200

    naujoves

--- atsakimai.php
+++ atsakimai.php
@@ -1,16 +1,21 @@
 <?php
 $failoVieta = $_SERVER['DOCUMENT_ROOT']."\Dilanas.txt";
-$failas = fopen($failoVieta, "r");
-if($failas === FALSE)
+$lines = file($failoVieta);
+foreach ($lines as $line_num => $line)
 {
-    echo "Nepavyko Atidaryti Failo";
+    echo "Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
 }
-else
-{
+// $failas = fopen($failoVieta, "r");
+// if($failas === FALSE)
+// {
+ //   echo "Nepavyko Atidaryti Failo";
+//}
+//else
+//{
 // Jei Pavyko, į failą įrašom kažkokią informaciją: 
 // Sitoj vietoj skaito faila po viena eilute
 // Jeigu eiluteje yra zodis kazkoksai Karina ta eilute atspauzdinti toliau nuo krasto tarp " "
 // Jeigu neteisingas isversti i  ( )
-fclose($failas);
-}
+//fclose($failas);
+//}
 ?>
