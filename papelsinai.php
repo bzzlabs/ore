4de4df99edf96fff7fa4c1a523ad3601a8068ded
Donatas <anushauskas@gmail.com>
2018-01-14 18:26:53 +0200

    l

---
+++ papelsinai.php
@@ -0,0 +1,22 @@
+<?php
+$failoVieta = $_SERVER['DOCUMENT_ROOT']."\\vaisiai.txt";
+//$failas = fopen($failoVieta, "r");
+$lines = file($failoVieta);
+
+foreach ($lines as $line_num => $line) {
+    $pos = strpos($line, 'apelsinas');
+    if ($pos !== FALSE){
+        echo $line."<br />\n"; 
+    } 
+    //else{
+      //  echo "(".'Karina'.") <br />\n";
+    //}
+    //echo "Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
+}
+    
+// Jei Pavyko, į failą įrašom kažkokią informaciją: 
+// Sitoj vietoj skaito faila po viena eilute
+// Jeigu eiluteje yra zodis kazkoksai Karina ta eilute atspauzdinti toliau nuo krasto tarp " "
+// Jeigu neteisingas isversti i  ( )
+//fclose($failas);
+?>
