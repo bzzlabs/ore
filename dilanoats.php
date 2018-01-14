0383098a7942eca5799e44240cffd094e2f9f70c
Dilanas <dila.makn@gmail.com>
2018-01-14 18:54:54 +0000

    as

--- dilanoats.php
+++ dilanoats.php
@@ -3,17 +3,19 @@ $failoVieta = $_SERVER['DOCUMENT_ROOT']."\\vaisiai.txt";
 //$failas = fopen($failoVieta, "r");
 $lines = file_get_contents($failoVieta);
 echo $lines;
-// foreach ($lines as $line_num => $line) {
-    // $pos = strpos($line, 'apelsinas');
-    // if ($pos === FALSE){
+$lines2 = explode("\r\n", $lines);
+echo $lines2;
+/* foreach ($lines as $line_num => $line) {
+     $pos = strpos($line, 'apelsinas');
+     if ($pos === FALSE){
 
-    // } 
-    // else{
-       // echo $line."<br />\n";
-    //}
+    } 
+    else{
+       echo $line."<br />\n";
+    }
     //echo "Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
-// }
-    
+}
+  */  
 // Jei Pavyko, į failą įrašom kažkokią informaciją: 
 // Sitoj vietoj skaito faila po viena eilute
 // Jeigu eiluteje yra zodis kazkoksai Karina ta eilute atspauzdinti toliau nuo krasto tarp " "
